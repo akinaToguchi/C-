a5cb352047eaa8392978ecd3ee3a73be2d076c5c
akinaToguchi <122374165+akinaToguchi@users.noreply.github.com>
2023-01-17 09:52:08 -0300

    Add files via upload

---
+++ Modulo 1/Desafio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool n1 = true, n2 = false, n3 = true;
+            Console.WriteLine(" n1 ^ n2= " + (n1 ^ n2));
+            Console.WriteLine("(n1 & !n2) | n3= " + ((n1 & !n2) | n3));
+            Console.WriteLine("(n1 | n2) & !n3= " + ((n1 | n2) & !n3));
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modulo 1/Desafio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 5, n2 = 10;
+            Console.WriteLine("n1 es igual a:" + n1);
+            Console.WriteLine("n2 es igual a: " + n2);
+            Console.WriteLine("n1 mas n2:" + (n1 + n2));
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
